151b0869e8f0fba7d47a25588ccdd351e900c966
Mert  Ozkaynak <mertozkaynak@windowslive.com>
2020-03-30 23:31:58 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -2,13 +2,13 @@
 require 'pry'
 def generate_star_date
   
-star_date = (rand(100000) + 400000) / 10.0
+    (rand(100000) + 400000) / 10.0
 end
 
 
-def state_log(star_date)
+def state_log(generate_star_date)
 
-  "Captain's Log, star date #{star_date}."
+  "Captain's Log, star date #{generate_star_date}."
   
 end
 binding.pry 
